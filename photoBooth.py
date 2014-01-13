5891d298ff305aac53268c567f7d8d9235ca6e77
Kyle Champlin <k.champlin@gmail.com>
2014-01-13 14:16:49 -0800

    initial test of raspberry pi photobooth

---
+++ photoBooth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import time 
+import picamera
+import RPi.GPIO as GPIO
+import subprocess
+
+pushBtn = 23
+imgPath = './images/'
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pushBtn, GPIO.IN)
+
+def createPixStrip():
+    global imgPath
+    Proc = subprocess.Popen(['montage', './images/pic0.jpg', './images/pic1.jpg', './images/pic2.jpg', '-mode', 'Concatenate', '-tile', '1x3', './images/montage.jpg'])
+
+def sayCheese():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024,768)
+	global imgPath
+        for i in range(0,3):
+            camera.start_preview()
+            # camera needs to "warm up"
+            time.sleep(4)
+            photoFile = imgPath + 'pic' + str(i) + '.jpg'
+            print "Take Picture " + str(i)  
+	    camera.capture(photoFile)
+
+
+while True:
+    if ( GPIO.input(pushBtn) == False ):
+        print "Button Pressed\n Taking Picture"
+        sayCheese()
+        createPixStrip()  
+    else:
+        print "Button Open"
+
+    time.sleep(1.0);
+    
+
