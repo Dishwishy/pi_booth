009a4af944220c159cdcd09553c7c3bab41c5453
Kyle Champlin <k.champlin@gmail.com>
2014-01-15 14:32:48 -0800

    updated file to call opengl command for countdown timer

--- photoBooth.py
+++ photoBooth.py
@@ -4,27 +4,29 @@ import picamera
 import RPi.GPIO as GPIO
 import subprocess
 
-#using the GPIO 23 pin as marked on the 
-#adafruit breakout board
 pushBtn = 23
 imgPath = './images/'
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pushBtn, GPIO.IN)
 
+def createPixStrip():
+    global imgPath
+    Proc = subprocess.Popen(['montage', './images/pic0.jpg', './images/pic1.jpg', './images/pic2.jpg', '-mode', 'Concatenate', '-tile', '1x3', './images/montage.jpg'])
+
+def countDown():
+    Proc = subprocess.Popen(['./hello_font.bin'])
+
 def sayCheese():
     with picamera.PiCamera() as camera:
-<<<<<<< HEAD
-        camera.resolution = (800,600)
-=======
     	#will need to change this after testing 
     	#different picture sizes
     
         camera.resolution = (1024,768)
->>>>>>> 4166cd78baba7ab4f6f92b19a53824ac1358e4fe
 	global imgPath
         for i in range(0,3):
             camera.start_preview()
+	    countDown()
             # camera needs to "warm up"
             time.sleep(4)
             photoFile = imgPath + 'pic' + str(i) + '.jpg'
